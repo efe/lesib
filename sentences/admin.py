4241531a5d87bf0b574b705215a5f506832e3c86
Efe Öge <efe@oge.me>
2016-12-25 15:31:36 +0300

    Changes on Sentence Admin

--- sentences/admin.py
+++ sentences/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from .models import Sentence
+from sentences.models import Sentence
+from words.models import Word
 
 
-admin.site.register(Sentence)
+class WordsInline(admin.TabularInline):
+    model = Word
+    readonly_fields = ('content', 'stem', 'content_type', 'stem_type', 'morphology', 'position', 'tag', 'numbering')
+    max_num = 0
+    can_delete = False
+
+
+class SentenceAdmin(admin.ModelAdmin):
+    inlines = [
+        WordsInline,
+    ]
+    readonly_fields = ('question', 'query_body')
+admin.site.register(Sentence, SentenceAdmin)
