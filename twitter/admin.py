1210b6375fb50e61e81561156cdeb3586dce1b8c
Efe Öge <efe@oge.me>
2016-12-27 13:16:06 +0300

    Twitter API method is modified

--- twitter/admin.py
+++ twitter/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
-from twitter.models import Tweet
+
+from sentences.models import Sentence
+from .models import Tweet
 
 admin.site.register(Tweet)
+
+
+# class SentenceInline(admin.TabularInline):
+#     model = Sentence
+#     readonly_fields = ('content', 'question',)
+#     max_num = 0
+#     can_delete = False
+#
+#
+# class TweetAdmin(admin.ModelAdmin):
+#     inlines = [
+#         SentenceInline,
+#     ]
+#
+# admin.site.register(Tweet, TweetAdmin)
--- twitter/models.py
+++ twitter/models.py
@@ -1,23 +1,24 @@
 import json
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from TwitterAPI import TwitterAPI
 from keys.TWITTER_KEY import access_token_secret, access_token, consumer_secret, consumer_key
 from sentences.models import Sentence
 
 
 class Tweet(models.Model):
-    url = models.URLField()
-    sentence = models.ForeignKey('sentences.Sentence', related_name="tweet")
+    tweet_id = models.CharField(_("Id of Tweet"), max_length=255, blank=True, help_text="https://twitter.com/efeoge/status/748450479600959488 için 748450479600959488")
+    sentence = models.ForeignKey('sentences.Sentence', related_name="tweet", blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.sentence
+        return self.sentence.content
 
     def save(self, *args, **kwargs):
         api = TwitterAPI(consumer_key, consumer_secret, access_token, access_token_secret)
-        r = api.request('statuses/show/:%d' % 210462857140252672)
+        r = api.request('statuses/show/:%s' % self.tweet_id)
         d = json.loads(r.text)
         d = d["text"]
-        sentence = Sentence.objects.create(content=d)
+        self.sentence = Sentence.objects.create(content=d)
         super(Tweet, self).save(*args, **kwargs)
